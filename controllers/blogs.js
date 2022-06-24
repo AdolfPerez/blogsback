75deeafcf7b9501a06245f673718c057dcd8eb79
Adolfo <aapc972@gmail.com>
2022-06-23 21:07:09 -0400

    actualizando

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -34,4 +34,26 @@ blogsRouter.post('/', async ({body}, response, next) => {
   }
 })
 
+blogsRouter.delete('/:id', async ({params}, response) => {
+  await Blog.findByIdAndRemove(params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+
+  Blog.findByIdAndUpdate(request.params.id, 
+    { 
+      title: request.body.title, 
+      author: request.body.author, 
+      url: request.body.url, 
+      likes: request.body.likes
+    }, 
+    { new: true }
+  )
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+    .catch(error => next(error))
+})
+
 module.exports = blogsRouter
--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -66,4 +66,23 @@ test('verificar que la respuesta es 400 si faltan las propiedades title y url', 
     .send(newBlog)
     .expect(400)
 })
+test('succeeds with status code 204 if id is valid', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(
+    helper.initialBlogs.length - 1
+  )
+
+  for(let blog of blogsAtEnd) {
+    expect(blog).not.toEqual(blogToDelete)
+  }
+})
+
 afterAll(() => mongoose.connection.close())
